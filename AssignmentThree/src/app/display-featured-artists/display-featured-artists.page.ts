7033c067c43392b825ac666ba03b70e7bcd8da85
ishangiri <81349942+ishangiri@users.noreply.github.com>
2024-04-07 20:17:24 +1000

    display featured artist done.
    Finished display featured artists

--- AssignmentThree/src/app/display-featured-artists/display-featured-artists.page.ts
+++ AssignmentThree/src/app/display-featured-artists/display-featured-artists.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/dataService';
+import { Artist } from 'src/Artist';
 
 @Component({
   selector: 'app-display-featured-artists',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayFeaturedArtistsPage implements OnInit {
 
-  constructor() { }
+  featuredArtists: Artist[] = [];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
-
+//function to get artists from api and assign it to the empty artists array.
+loadFeaturedArtists(): void {
+  this.dataService.getAllArtists().subscribe(
+    (response: any) => {
+      // Check if the response is an array
+      if (Array.isArray(response)) {
+        // If it is, assign it to artists
+        let isFeaturedArtists = response.filter(artist => artist.is_featured_artist === 1);
+        this.featuredArtists = isFeaturedArtists;
+      } else {
+        // If not, log an error
+        console.error('Unexpected error:', response);
+      }
+    },
+    (error) => {
+      console.log('Error fetching featured artists:', error);
+    }
+  );
+}
 }
